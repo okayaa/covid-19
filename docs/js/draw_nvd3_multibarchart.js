7829e0b42d9f50fe085d09a61ae8ccce4c27d89c
okayaa <1.959964@gmail.com>
2022-12-07 21:47:35 +0900

    Refactor the function

--- docs/js/draw_nvd3_multibarchart.js
+++ docs/js/draw_nvd3_multibarchart.js
@@ -1,23 +1,21 @@
-
-const endpoints = ["newly_confirmed_cases_daily", "severe_cases_daily", "number_of_deaths_daily"]; // , "requiring_inpatient_care_etc_daily"];
-
-const all_data = endpoints.map(endpoint => fetch("dat/" + endpoint + ".json").then(response => {return(response.json())}));
-
-const endpoints_J = ["新規陽性者数", "重症者数", "死亡者数"]; // , "入院治療等を要する者数"];
-
-const prefectures_J = ['全国合計', '北海道', '青森県', '岩手県', '宮城県', '秋田県', '山形県', 
-                     '福島県', '茨城県', '栃木県', '群馬県', '埼玉県', '千葉県', '東京都', 
-                     '神奈川県', '新潟県', '富山県', '石川県', '福井県', '山梨県', '長野県', 
-                     '岐阜県', '静岡県', '愛知県', '三重県', '滋賀県', '京都府', '大阪府', 
-                     '兵庫県', '奈良県', '和歌山県', '鳥取県', '島根県', '岡山県', '広島県', 
-                     '山口県', '徳島県', '香川県', '愛媛県', '高知県', '福岡県', '佐賀県', 
-                     '長崎県', '熊本県', '大分県', '宮崎県', '鹿児島県', '沖縄県'];
-
 const time_format_d3 = "%Y/%-m/%-d"; // "%d%b%Y"
 const time_format_moment = "YYYY/M/D"; // "DDMMMYYYY"
 
-Promise.all(all_data).then(
-  function (all_data) {
+const prefectures_J = ['全国合計', '北海道', '青森県', '岩手県', '宮城県', '秋田県', '山形県', 
+                       '福島県', '茨城県', '栃木県', '群馬県', '埼玉県', '千葉県', '東京都', 
+                       '神奈川県', '新潟県', '富山県', '石川県', '福井県', '山梨県', '長野県', 
+                       '岐阜県', '静岡県', '愛知県', '三重県', '滋賀県', '京都府', '大阪府', 
+                       '兵庫県', '奈良県', '和歌山県', '鳥取県', '島根県', '岡山県', '広島県', 
+                       '山口県', '徳島県', '香川県', '愛媛県', '高知県', '福岡県', '佐賀県', 
+                       '長崎県', '熊本県', '大分県', '宮崎県', '鹿児島県', '沖縄県'];
+
+const endpoints_J = ["新規陽性者数", "重症者数", "死亡者数"]; // , "入院治療等を要する者数"];
+const endpoints = ["newly_confirmed_cases_daily", "severe_cases_daily", "number_of_deaths_daily"]; // , "requiring_inpatient_care_etc_daily"];
+
+const promise_all_data = endpoints.map(endpoint => fetch("dat/" + endpoint + ".json").then(response => {return(response.json())}));
+
+Promise.all(promise_all_data)
+  .then(function (all_data) {
 
     // add the options to the button
     d3.select("#endpoint")
@@ -273,7 +271,5 @@ Promise.all(all_data).then(
       }, change_period);
 
       return chart;
-
     });
-
   });
